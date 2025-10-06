017de226c4b7a3c7aeaa1c49fb7b2632ebe3bfa4
sohir <p@gmail.com>
2025-10-06 20:38:53 +0300

    updated

--- index.js
+++ index.js
@@ -2,22 +2,17 @@ import express from "express";
 import userRouter from "./src/modules/user/user.routes.js";
 import { dbConnection } from "./database/dbConnections.js";
 import noteRouter from "./src/modules/note/note.routes.js";
-import { User } from "./database/models/user.model.js";
-import jwt from 'jsonwebtoken'
 import dotenv from "dotenv";
 
 const app = express()
 const port = 3000
 dotenv.config({ quiet: true });
-
 dbConnection()
 
 app.use(express.json())
 
 app.use('/auth', userRouter)
 app.use('/notes', noteRouter)
-
-
 
 app.get('/',(req,res,next)=>{
     res.send("Hello World")
